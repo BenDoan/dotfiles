460f25e4426d6e602862a80f68bfb699e156b4c4
BenDoan <ben@simcaster.net>
2012-05-10 22:21:40 -0500

    finished install script

---
+++ install.sh
@@ -0,0 +1,11 @@
+mkdir ~/.config/
+
+#Directories
+ln -s "`pwd`/awesome/" ~/.config/
+ln -s "`pwd`/.oh-my-zsh/" ~/
+ln -s "`pwd`/.vim/" ~/
+
+#Files
+ln -s "`pwd`/.vimrc" ~/
+ln -s "`pwd`/.xinitrc" ~/
+ln -s "`pwd`/.zshrc" ~/
