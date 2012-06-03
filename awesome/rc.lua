cd0691ffb5d83e61d4942a7d5a9a714d0f0024e5
BenDoan <ben@simcaster.net>
2012-06-02 22:04:06 -0500

    added new layouts to awesome wm

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -53,11 +53,11 @@ modkey = "Mod4"
 layouts =
 {
     awful.layout.suit.tile,
-    --awful.layout.suit.tile.left,
-    --awful.layout.suit.tile.bottom,
-    --awful.layout.suit.tile.top,
-    --awful.layout.suit.fair,
-    --awful.layout.suit.fair.horizontal,
+    awful.layout.suit.tile.left,
+    awful.layout.suit.tile.bottom,
+    awful.layout.suit.tile.top,
+    awful.layout.suit.fair,
+    awful.layout.suit.fair.horizontal,
     --awful.layout.suit.spiral,
     --awful.layout.suit.spiral.dwindle,
     awful.layout.suit.max,
