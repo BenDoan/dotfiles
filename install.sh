09d282b6309530aee36a6acfbeb9655a1a71f722
Ben Doan <ben@bendoan.me>
2013-05-20 13:25:28 -0500

    updated install.sh

--- install.sh
+++ install.sh
@@ -1,28 +1,45 @@
 #!/bin/bash
 
 function link {
-    ln -s "`pwd`/$1" ~/
+    if [[ ! -h ~/${1%/} ]]; then # removes trailing slashes
+        ln -s "`pwd`/$1" ~/
+        echo "Linking ${1}"
+    fi
+
+    if [[ -f ~/$1 && ! -h ~/$1 ]]; then
+        echo "Error: regular file exists for $1"
+    fi
+}
+
+function link_config {
+    if [[ ! -h ~/.config/${1%/} ]]; then # removes trailing slashes
+        ln -s "`pwd`/$1" ~/.config/
+        echo "Linking ${1}"
+    fi
+
+    if [[ -f ~/.config/$1 && ! -h ~/.config/$1 ]]; then
+        echo "Error: regular file exists for $1"
+    fi
 }
 
 
+if [[ ! -d ~/.config ]]; then
+    mkdir ~/.config/
+fi
+
 #Directories
-mkdir ~/.config/
+link_config 'awesome/'
+link_config 'fish/'
 
-ln -s "`pwd`/awesome/" ~/.config/
-ln -s "`pwd`/fish/" ~/.config/
-
-link '.oh-my-zsh/'
 link '.vim/'
 link '.i3/'
 link '.fonts/'
 
 #Files
 link '.vimrc'
-link '.xinitrc'
 link '.xsession'
 link '.zshrc'
 link '.conkyrc'
-link '.Xresources'
 link '.tmux.conf'
 link '.i3status.conf'
 
