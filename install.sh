1bbb60d0e622e599c1e1ccc3996618df3111e2d4
Ben Doan <ben@bendoan.me>
2013-05-22 13:16:25 -0500

    removed dots from file names

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 function link {
-    if [[ ! -h ~/${1%/} ]]; then # removes trailing slashes
-        ln -s "`pwd`/$1" ~/
+    if [[ ! -h ~/.${1%/} ]]; then # removes trailing slashes
+        ln -s "`pwd`/$1" ~/.$1
         echo "Linking ${1}"
     fi
 
-    if [[ -f ~/$1 && ! -h ~/$1 ]]; then
+    if [[ -f ~/.$1 && ! -h ~/.$1 ]]; then
         echo "Error: regular file exists for $1"
     fi
 }
@@ -31,17 +31,17 @@ fi
 link_config 'awesome/'
 link_config 'fish/'
 
-link '.vim/'
-link '.i3/'
-link '.fonts/'
+link 'vim'
+link 'i3'
+link 'fonts'
 
 #Files
-link '.vimrc'
-link '.xsession'
-link '.zshrc'
-link '.conkyrc'
-link '.tmux.conf'
-link '.i3status.conf'
+link 'vimrc'
+link 'xsession'
+link 'zshrc'
+link 'conkyrc'
+link 'tmux.conf'
+link 'i3status.conf'
 
 echo "==============="
 echo "Dotfiles linked"
