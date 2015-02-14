c3eca344b45837742ea69942441335da7524e252
Ben Doan <ben@bendoan.me>
2015-02-13 21:24:29 -0600

    updated path

--- arch_fresh_install.sh
+++ arch_fresh_install.sh
@@ -1,4 +1,5 @@
 programs="conky roxterm htop ntp emacs libreoffice rsync virtualbox virtualbox-host-modules zenity zsh gvim meld firefox chromium python2 python2-pip vlc curl tmux dmenu i3 acpi ImageMagick mosh git"
+aur_programs="rofi" #not used currently
 
 ~/dotfiles/manage.sh install
 git clone https://github.com/BenDoan/scripts ~/scripts
