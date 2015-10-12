42c9bec97bb82769c6b6bf2363166173c6334f3c
Ben Doan <ben@bendoan.me>
2015-10-12 16:29:43 -0500

    updated arch setup script

--- arch_fresh_install.sh
+++ arch_fresh_install.sh
@@ -1,4 +1,4 @@
-programs="conky roxterm htop ntp emacs libreoffice rsync virtualbox virtualbox-host-modules zenity zsh gvim meld firefox chromium python2 python2-pip vlc curl tmux dmenu i3 acpi ImageMagick mosh git"
+programs="python base-devel conky roxterm htop ntp emacs libreoffice rsync virtualbox virtualbox-host-modules zenity zsh gvim meld firefox chromium python2 python2-pip vlc curl tmux dmenu i3 acpi ImageMagick mosh git"
 aur_programs="rofi" #not used currently
 
 ~/dotfiles/manage.sh install
