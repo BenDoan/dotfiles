f3b284bdfa00187c31e8d1892856660468de1349
Ben Doan <bdoan@linkedin.com>
2018-06-14 13:57:04 -0700

    enable 256 color mode in tmux for fish

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -3,6 +3,8 @@ set fish_greeting
 set -x FZF_DEFAULT_COMMAND "rg --files"
 set -x FZF_CTRL_T_COMMAND "$FZF_DEFAULT_COMMAND"
 
+alias tmux "tmux -2"
+
 function fish_prompt
   set -l last_status $status
 
