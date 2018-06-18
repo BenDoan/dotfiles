a1e245dfe2abb375ae340477d6d91ca97425dc4f
Ben Doan <bdoan@linkedin.com>
2018-06-18 08:56:47 -0700

    ported functions from zsh to fish

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,9 +1,28 @@
 set fish_greeting
 
-set -x FZF_DEFAULT_COMMAND "rg --files"
+set -x FZF_DEFAULT_COMMAND 'rg --files'
 set -x FZF_CTRL_T_COMMAND "$FZF_DEFAULT_COMMAND"
 
-alias tmux "tmux -2"
+alias tmux 'tmux -2'
+alias ls 'ls -hF --color=auto --group-directories-first'
+alias rg "rg -S"
+alias gpm 'git push origin master'
+
+function difflines
+  git diff --no-index $1 $2
+end
+
+function diffchars
+  git diff --color-words=. --no-index $1 $2
+end
+
+function diffwords
+  git diff --color-words='[^[:space:]]|([[:alnum:]]|UTF_8_GUARD)+' --no-index $1 $2
+end
+
+function git-ammend-modified
+  git add (git diff --name-only); and git commit --amend --no-edit
+end
 
 function fish_prompt
   set -l last_status $status
@@ -20,7 +39,18 @@ function fish_right_prompt -d "Write out the right prompt"
     echo -n " "
     set_color normal
 end
+#
+# Turn virtualenvs on/off
+function von
+    set dirname (basename (pwd))
+    if [[ -n $dirname && -d $HOME/envs/$dirname ]]; then
+        source $HOME/envs/$dirname/bin/activate
+    end
+end
+function voff
+  deactivate
+end
 
-if type -q nvim 2> /dev/null
+if type -q nvim
     alias vim "nvim"
 end
