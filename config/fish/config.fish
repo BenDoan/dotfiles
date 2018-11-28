741582b00bcbded8807599d27e322417e3ebb678
Ben Doan <bdoan@linkedin.com>
2018-11-28 15:05:03 -0800

    add fish aliases

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,6 +2,7 @@ set fish_greeting
 
 set -x FZF_DEFAULT_COMMAND 'rg --files'
 set -x FZF_CTRL_T_COMMAND "$FZF_DEFAULT_COMMAND"
+set -x EDITOR "nvim"
 
 alias tmux 'tmux -2'
 alias ls 'ls -hF --color=auto --group-directories-first'
@@ -9,19 +10,23 @@ alias rg "rg -S"
 alias gpm 'git push origin master'
 
 function difflines
-  git diff --no-index $1 $2
+  git diff --no-index $argv[1] $argv[2]
 end
 
 function diffchars
-  git diff --color-words=. --no-index $1 $2
+  git diff --color-words=. --no-index $argv[1] $argv[2]
 end
 
 function diffwords
-  git diff --color-words='[^[:space:]]|([[:alnum:]]|UTF_8_GUARD)+' --no-index $1 $2
+  git diff --color-words='[^[:space:]]|([[:alnum:]]|UTF_8_GUARD)+' --no-index $argv[1] $argv[2]
 end
 
 function git-ammend-modified
   git add (git diff --name-only); and git commit --amend --no-edit
+end
+
+function git-set-upstream-to-master
+  git branch --set-upstream-to=origin/master (git rev-parse --abbrev-ref HEAD)
 end
 
 function fish_prompt
@@ -51,6 +56,14 @@ function voff
   deactivate
 end
 
+function eval_ssh_agent
+    eval (command ssh-agent -c | sed -E 's/^setenv (.+);$/set \1; set -Ux \1;/')
+end
+
 if type -q nvim
     alias vim "nvim"
+end
+
+if [ -f "$HOME/.config.local.fish" ]
+  source $HOME/.config.local.fish
 end
