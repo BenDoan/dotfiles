d6fd793a7b164904c82776a83c6dc06a00c2d49c
Ben Doan <ben@bendoan.me>
2021-03-21 12:54:05 -0500

    remove z installation from setup

--- _setup.sh
+++ _setup.sh
@@ -5,10 +5,6 @@ mkdir ~/downloads ~/documents ~/music ~/pictures ~/shots ~/bin >& /dev/null
 
 ln -s $HOME/.xsession $HOME/.xinitrc >& /dev/null
 
-if [[ ! -d ~/bin/z ]]; then
-    git clone https://github.com/rupa/z.git ~/bin/z
-fi
-
 if [[ ! -d ~/.fzf ]]; then
     git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
     ~/.fzf/install
