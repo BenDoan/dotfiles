df39115ecef16d6f435363f4ceda580a322f254e
Ben Doan <ben@bendoan.me>
2021-07-23 22:09:12 -0500

    add arch-install script

---
+++ arch-install.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+sudo pacman -S \
+  xclip \
+  ttf-freefont \
+  ttf-arphic-uming \
+  ttf-baekmuk \
+  adobe-source-han-sans-jp-fonts \
+  adobe-source-han-serif-jp-fonts \
+  ttf-hanazono \
+  dunst \
+  picom \
+  vlc \
+  mplayer \
+  youtube-dl \
+  i3lock \
+  rustup \
+  neovim \
+  base-devel \
+  automake \
+  autoconf \
+  ripgrep \
+  man \
+  volumeicon \
+  alsa-utils \
+  alsa \
+  pulseaudio-alsa \
+  pulseaudio \
+  pass \
+  fish \
+  xterm \
+  wget \
+  openssh \
+  rsync \
+  conky \
+  fakeroot
